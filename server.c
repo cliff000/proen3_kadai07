1779914aac2ec11449e2937dba3c0aec4b7e3bbd
Daishi Matsui <d.matsui.220@ms.saitama-u.ac.jp>
2019-06-12 21:53:17 +0900

    finish

--- server.c
+++ server.c
@@ -12,6 +12,8 @@
 
 void *recvText(void *arg);
 void *sendText(void *arg);
+void *recvUserName(void *arg);
+void *sendUserName(void *arg);
 
 void chop(char *str)
 {
@@ -23,6 +25,8 @@ void chop(char *str)
 
 int connected_socket; // accept()が返すファイル識別子
 int exitFlag = 0; //終了フラグ
+char myName[BUFSIZE]; //自分の名前
+char opponentName[BUFSIZE]; //相手の名前
 
 int main(int argc, char *argv[])
 {
@@ -97,46 +101,104 @@ int main(int argc, char *argv[])
 
 
 
-    // スレッド作成
-    pthread_t th1,th2;
+    //ユーザー名送受信
+    pthread_t recvName,sendName;
     void *rval;
 
     srand((unsigned int)time(NULL));
 
-    //スレッドth1を生成し、recvText("th1")を呼び出す
-    if(pthread_create(&th1, NULL, recvText, (void *) "th1") != 0){
+    //スレッドrecvNameを生成し、recvUserName("recvName")を呼び出す
+    if(pthread_create(&recvName, NULL, recvUserName, (void *) "recvName") != 0){
+        perror("Thread creation failed.\n");
+        exit(EXIT_FAILURE);
+    }
+    //スレッドsendNameを生成し、sendUserName("sendName")を呼び出す
+    if(pthread_create(&sendName, NULL, sendUserName, (void *) "sendName") != 0){
         perror("Thread creation failed.\n");
         exit(EXIT_FAILURE);
     }
 
-    //スレッドth2を生成し、sendText("th2")を呼び出す
-    if(pthread_create(&th2, NULL, sendText, (void *) "th2") != 0){
+    //スレッドrecvNameの終了を待つ
+    if(pthread_join(recvName, &rval) != 0){
+        perror("Failed to join with recvName.\n");
+    }else{
+        free(rval);
+    }
+    //スレッドsendNameの終了を待つ
+    if(pthread_join(sendName, &rval) != 0){
+        perror("Failed to join with sendName.\n");
+    }else{
+        free(rval);
+    }
+
+
+
+    printf("--------------- chat start ---------------\n");
+
+
+
+    // スレッド作成
+    pthread_t recv,send;
+
+
+    srand((unsigned int)time(NULL));
+
+    //スレッドrecvを生成し、recvText("recv")を呼び出す
+    if(pthread_create(&recv, NULL, recvText, (void *) "recv") != 0){
+        perror("Thread creation failed.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //スレッドsendを生成し、sendText("send")を呼び出す
+    if(pthread_create(&send, NULL, sendText, (void *) "send") != 0){
         perror("Thread creation failed.\n");
         exit(EXIT_FAILURE);
     }
 
 
-    //スレッドth1の終了を待つ
-    if(pthread_join(th1, &rval) != 0){
-        perror("Failed to join with recv.\n");
-    }else{
-        printf("finished recv (thread ID = %p)\n", (void*)*(pthread_t*)rval);
-        free(rval);
+    //送受信のスレッドが終わるまで待つ
+    while(exitFlag == 0){
+        if(pthread_tryjoin_np(recv, &rval) == 0)
+            exitFlag = 1;
+        if(pthread_tryjoin_np(send, &rval) == 0)
+            exitFlag = 1;
     }
+    free(rval);
+    printf("\n---------------  finish  ---------------\n");
 
-    //スレッドth2の終了を待つ
-    if(pthread_join(th2, &rval) != 0){
-        perror("Failed to join with send.\n");
-    }else{
-        printf("finished send (thread ID = %p)\n", (void*)*(pthread_t*)rval);
-        free(rval);
-    }
 
     return 0;
 }
 
 
+//ユーザー名受信を行う関数
+void *recvUserName(void *arg){
+    pthread_t *thread_id = (pthread_t *) malloc(sizeof(pthread_t));
+    *thread_id = pthread_self(); // 自分のスレッド番号を得る
 
+    memset(opponentName, '\0', BUFSIZE);
+    recv(connected_socket, opponentName, BUFSIZE,0);
+
+    pthread_exit((void *) thread_id);
+}
+//ユーザー名送信を行う関数
+void *sendUserName(void *arg){
+    pthread_t *thread_id = (pthread_t *) malloc(sizeof(pthread_t));
+    *thread_id = pthread_self(); // 自分のスレッド番号を得る
+
+    memset(myName, '\0', BUFSIZE);
+    printf("Please tell me your name\n>");
+    if (fgets(myName, BUFSIZE, stdin) == NULL)
+        strcpy(myName, "cliant");
+    chop(myName);
+    //データ送信
+    send(connected_socket, myName, BUFSIZE,0);
+
+    pthread_exit((void *) thread_id);
+}
+
+
+// 受信を行う関数
 void *recvText(void *arg){
     char buffer[BUFSIZE]; //メッセージを格納するバッファ
 
@@ -148,15 +210,11 @@ void *recvText(void *arg){
         memset(buffer, '\0', BUFSIZE);
         // データ受信
         recv(connected_socket, buffer, BUFSIZE,0);
-        printf("\nfrom client: %s\n", buffer);
-        printf(">");
+        printf("\nfrom %s: %s\n", opponentName, buffer);
+        printf("%s>", myName);
         fflush(stdout);
-        if (strcmp(buffer, "quit") == 0){
-            if(exitFlag == 0){
-                exitFlag = 1;
-                send(connected_socket, "quit", BUFSIZE,0);
-            }
-        }
+        if (strcmp(buffer, "quit") == 0)
+            break;
 
         if(exitFlag)
             break;
@@ -165,6 +223,7 @@ void *recvText(void *arg){
     pthread_exit((void *) thread_id);
 }
 
+// 送信を行う関数
 void *sendText(void *arg){
     char buffer[BUFSIZE]; //メッセージを格納するバッファ
 
@@ -174,14 +233,14 @@ void *sendText(void *arg){
     while (1) {
         // 送信データを読み込む
         memset(buffer, '\0', BUFSIZE);
-        printf(">");
-        if (fgets(buffer, BUFSIZE, stdin) == NULL || exitFlag)
+        printf("%s>", myName);
+        if (fgets(buffer, BUFSIZE, stdin) == NULL)
             strcpy(buffer, "quit");
         chop(buffer);
         //データ送信
         send(connected_socket, buffer, BUFSIZE,0);
         if (strcmp(buffer, "quit") == 0)
-            exitFlag = 1;
+            break;
 
         if(exitFlag)
             break;
